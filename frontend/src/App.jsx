78780a5e7cc5059e379be25e76aa675a6220d03c
jinexus1 <ap2929470@gmail.com>
2024-08-18 14:17:29 +0530

    login page and landing page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,5 @@
 // import React from "react";
-import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Login from "./Pages/LoginPage/Login";
 import Register from "./Pages/Register/Register";
 function App() {
