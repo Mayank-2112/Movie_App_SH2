9d5504ac8f28b0e9a9eed154fa3cf1d638e06799
Atulit95 <atulitgupta436@gmail.com>
2024-08-20 20:05:47 +0530

    login page final UI

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -14,7 +14,7 @@ function App() {
       <div className="App">
         {/* <NavBar onSearch={handleSearch} /> */}
         <Routes>
-          <Route path="/" element={<Home />} />
+          {/* <Route path="/" element={<Home />} /> */}
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} />
         </Routes>
