f49aad3c2d1d09e4c28bca0557c12119a77106cb
Vecforge <devansh.ps002@gmail.com>
2024-08-23 23:24:19 +0530

    Update

--- frontend/src/Pages/Home/Home.jsx
+++ frontend/src/Pages/Home/Home.jsx
@@ -1,7 +1,12 @@
 import React from "react";
 import Card from "../../components/Card/Card";
+import NavBar from "../../components/NavBar/NavBar";
 const Home = () => {
-  return <Card />;
+  return(
+    <>
+    <NavBar />
+    </>
+  );
 };
 
 export default Home;
