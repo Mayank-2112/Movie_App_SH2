1a62654c4e24c91c25bd19c349168917fd5d182f
jinexus1 <ap2929470@gmail.com>
2024-08-23 15:51:08 +0530

    new api keys

--- frontend/src/Pages/BookNow/BookNow.jsx
+++ frontend/src/Pages/BookNow/BookNow.jsx
@@ -1,34 +1,38 @@
 import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import "./BookNow.css";
-import opencage from 'opencage-api-client';
+import opencage from "opencage-api-client";
 import TheaterDropdown from "../../components/TheaterDropdown/TheaterDropdown";
 
 const BookNow = () => {
-  const [city, setCity] = useState('');
-  const [location, setLocation]= useState({
-    lat:'',lng:''
+  const [city, setCity] = useState("");
+  const [location, setLocation] = useState({
+    lat: "",
+    lng: "",
   });
-  
-  const [activeTime, setActiveTime] = useState(null);
-  const [showTimmings, setShowTimmings] = useState([]);
+
+  // const [activeTime, setActiveTime] = useState(null);
+  // const [showTimmings, setShowTimmings] = useState([]);
 
   const handleButtonClick = (e) => {
     setCity(e.target.value);
   };
 
-  const getGeoLocation = async (e)=>{
+  const getGeoLocation = async (e) => {
     e.preventDefault();
     console.log(city);
     try {
-      const data = await opencage.geocode({ q: city, key: import.meta.env.VITE_OPENCAGE_API_KEY });
+      const data = await opencage.geocode({
+        q: city,
+        key: import.meta.env.VITE_OPENCAGE_API_KEY,
+      });
       if (data.status.code === 200 && data.results.length > 0) {
         const place = data.results[0];
         setLocation({
           lat: place.geometry.lat,
-          lng: place.geometry.lng
+          lng: place.geometry.lng,
         });
-        getTheaters(place.geometry.lat,place.geometry.lng);
+        // getTheaters(place.geometry.lat,place.geometry.lng);
       } else {
         console.log("Status", data.status.message);
         console.log("total_results", data.total_results);
@@ -40,62 +44,64 @@ const BookNow = () => {
         console.log("become a customer: https://opencagedata.com/pricing");
       }
     }
-    
   };
-    
 
-    const handleactiveTheater = (idx) => {
-      // console.log(key, buttonValues.key);
-      setActiveTheater(idx);
-      const selectedTheater = theater[idx];
-    if (selectedTheater.showings && selectedTheater.showings.Standard) {
-      setShowTimmings(selectedTheater.showings.Standard.times);
-    } else {
-      setShowTimmings([]);
-      console.log("No showings available for the selected theater");
-    }
-    };
-    const handleTimmingButton = (key)=>{
-        setActiveTime(key);
+  // const handleactiveTheater = (idx) => {
+  //   // console.log(key, buttonValues.key);
+  //   setActiveTheater(idx);
+  //   const selectedTheater = theater[idx];
+  //   if (selectedTheater.showings && selectedTheater.showings.Standard) {
+  //     setShowTimmings(selectedTheater.showings.Standard.times);
+  //   } else {
+  //     setShowTimmings([]);
+  //     console.log("No showings available for the selected theater");
+  //   }
+  // };
+  // const handleTimmingButton = (key) => {
+  //   setActiveTime(key);
+  // };
 
-    }
-    
   return (
     <div className="main">
       <h1>Book Now</h1>
       <form onSubmit={getGeoLocation}>
-        <input type="text" placeholder=" city names" id="city" value={city} onChange={handleButtonClick} />
+        <input
+          type="text"
+          placeholder=" city names"
+          id="city"
+          value={city}
+          onChange={handleButtonClick}
+        />
         <button type="submit">Search</button>
       </form>
-      <TheaterDropdown lat={location.lat} lng={location.lng} city={city}/>
-
+      <TheaterDropdown lat={location.lat} lng={location.lng} city={city} />
 
       <footer>
         <Link to="/seating">
           <button>select seat</button>
         </Link>
       </footer>
-      <p>{city}
+      <p>
+        {city}
         {location.lat}
         {location.lng}
       </p>
+      <div></div>
       <div>
-      
-      </div>
-      <div>
-      {showTimmings && (
+        {/* {showTimmings &&
           showTimmings.map((timming, idx) => (
-            <button key={idx}
-            onClick={() => handleTimmingButton(idx)}
-            style={{
-              backgroundColor: activeTime === idx ? "blue" : "gray",
-              color: "white",
-              margin: "5px",
-            }}
+            <button
+              key={idx}
+              onClick={() => handleTimmingButton(idx)}
+              style={{
+                backgroundColor: activeTime === idx ? "blue" : "gray",
+                color: "white",
+                margin: "5px",
+              }}
             >
-              {timming.start_time}</button>
-          ))
-        )}
+              {timming.start_time}
+            </button> */}
+        {/* ))} */}
       </div>
     </div>
   );
--- frontend/src/components/TheaterDropdown/TheaterDropdown.jsx
+++ frontend/src/components/TheaterDropdown/TheaterDropdown.jsx
@@ -1,82 +1,90 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from "react";
 
-const TheaterDropdown = ({lat,lng,city}) => {
+const TheaterDropdown = ({ lat, lng, city }) => {
+  useEffect(() => {
+    if (lat && lng) {
+      getTheaters(lat, lng);
+    }
+  }, [lat, lng]);
 
-    useEffect(() => {
-        if (lat && lng) {
-          getTheaters(lat, lng);
-        }
-      }, [lat, lng]);
-
-    const [theater, setTheater] = useState([]);
-    const [activeTheater, setActiveTheater] = useState(false);
-    const currentDate = new Date();
+  const [theater, setTheater] = useState([]);
+  const [activeTheater, setActiveTheater] = useState(false);
+  const currentDate = new Date();
 
   const year = currentDate.getFullYear();
-  const month = String(currentDate.getMonth() + 1).padStart(2, '0');
-  const day = String(currentDate.getDate()).padStart(2, '0');
-  const hours = String(currentDate.getHours()).padStart(2, '0');
-  const minutes = String(currentDate.getMinutes()).padStart(2, '0');
-  const seconds = String(currentDate.getSeconds()).padStart(2, '0');
+  const month = String(currentDate.getMonth() + 1).padStart(2, "0");
+  const day = String(currentDate.getDate()).padStart(2, "0");
+  const hours = String(currentDate.getHours()).padStart(2, "0");
+  const minutes = String(currentDate.getMinutes()).padStart(2, "0");
+  const seconds = String(currentDate.getSeconds()).padStart(2, "0");
 
-  const filmId = '364605';
-  
-    const getTheaters = async (lat,lng)=>{
-      try{
-        const res = await fetch(`https://api-gate2.movieglu.com/cinemasNearby/?n=30`,{
-            method: 'GET',
-            headers:{
-              'client' : 'MOVI_227',
-              'x-api-key': import.meta.env.VITE_X_API_KEY,
-              'authorization': import.meta.env.VITE_AUTHORIZATION,
-              'territory': 'IN',
-              'api-version': 'v200',
-              'geolocation':`${lat};${lng}`,
-              'device-datetime': `${year}-${month}-${day}T${hours}:${minutes}:${seconds}Z`
-            },
+  const filmId = "364605";
 
-        });
-        const data = await res.json();
-        if (res.ok){
-          console.log(data.cinemas);
-          const filteredTheater = data.cinemas.filter(theat => theat.city === city);
-          setTheater(filteredTheater);
+  const getTheaters = async (lat, lng) => {
+    try {
+      const res = await fetch(
+        `https://api-gate2.movieglu.com/cinemasNearby/?n=30`,
+        {
+          method: "GET",
+          headers: {
+            client: "MOVI_225",
+            "x-api-key": import.meta.env.VITE_X_API_KEY,
+            authorization: import.meta.env.VITE_AUTHORIZATION,
+            territory: "IN",
+            "api-version": "v200",
+            geolocation: `${lat};${lng}`,
+            "device-datetime": `${year}-${month}-${day}T${hours}:${minutes}:${seconds}Z`,
+          },
         }
-        else{
-          console.error('HTTP error:', res.status, res.statusText);
-          console.error('Response body:', data);
-        }
-      }catch(error){
-        console.log(error.message);
-        
+      );
+      const data = await res.json();
+      if (res.ok) {
+        console.log(data.cinemas);
+        const filteredTheater = data.cinemas.filter(
+          (theat) => theat.city === city
+        );
+        setTheater(filteredTheater);
+      } else {
+        console.error("HTTP error:", res.status, res.statusText);
+        console.error("Response body:", data);
       }
-    };
-    const handleActiveTheater = (event) => {
-        const selectedTheaterId = event.target.value;
-        const selectedTheater = theater.find(theat => theat.cinema_id === selectedTheaterId);
-        
-        setActiveTheater(selectedTheaterId);
-    
-        if (selectedTheater && selectedTheater.showings && selectedTheater.showings.Standard) {
-          setShowTimmings(selectedTheater.showings.Standard.times);
-        } else {
-          setShowTimmings([]);
-          console.log('No showings available for the selected theater');
-        }
-      };
-    
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
+  const handleActiveTheater = (event) => {
+    const selectedTheaterId = event.target.value;
+    const selectedTheater = theater.find(
+      (theat) => theat.cinema_id === selectedTheaterId
+    );
+
+    setActiveTheater(selectedTheaterId);
+
+    if (
+      selectedTheater &&
+      selectedTheater.showings &&
+      selectedTheater.showings.Standard
+    ) {
+      setShowTimmings(selectedTheater.showings.Standard.times);
+    } else {
+      setShowTimmings([]);
+      console.log("No showings available for the selected theater");
+    }
+  };
+
   return (
     <>
-        <select id="theater" onChange={handleActiveTheater}>
+      <select id="theater" onChange={handleActiveTheater}>
         <option value="">Select a theater</option>
-        {theater && theater.map((theater, idx) => (
-          <option key={idx} value={theater.cinema_id}>
-            {theater.cinema_name}, {theater.address}
-          </option>
-        ))}
+        {theater &&
+          theater.map((theater, idx) => (
+            <option key={idx} value={theater.cinema_id}>
+              {theater.cinema_name}, {theater.address}
+            </option>
+          ))}
       </select>
     </>
-  )
-}
+  );
+};
 
-export default TheaterDropdown
+export default TheaterDropdown;
