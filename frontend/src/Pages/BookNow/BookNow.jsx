29f9b48a38d0252eba789e6662ca8429f24c736c
Mayank2112 <30808mayank21@gmail.com>
2024-08-23 11:48:11 +0530

    Theater fetched from api

--- frontend/src/Pages/BookNow/BookNow.jsx
+++ frontend/src/Pages/BookNow/BookNow.jsx
@@ -1,56 +1,155 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import "./BookNow.css";
+import opencage from 'opencage-api-client';
 
 const BookNow = () => {
-  const [theater, setTheater] = useState(false);
-  const [activeButton, setActiveButton] = useState(null);
+  const [city, setCity] = useState('');
+  const [location, setLocation]= useState({
+    lat:'',lng:''
+  });
+  const [theater, setTheater] = useState([]);
+  const [activeTheater, setActiveTheater] = useState(null);
+  const [activeTime, setActiveTime] = useState(null);
+  const [showTimmings, setShowTimmings] = useState([]);
 
-  const buttonValues = {
-    btn1: "9:00 AM - 12:00 PM",
-    btn2: "1:00 PM - 4:00 PM",
-    btn3: "5:00 PM - 8:00 PM",
-    btn4: "9:00 PM - 12:00 AM",
+  const handleButtonClick = (e) => {
+    setCity(e.target.value);
   };
 
-  const handleButtonClick = (key) => {
-    console.log(key, buttonValues.key);
-
-    setActiveButton(key);
+  const getGeoLocation = async (e)=>{
+    e.preventDefault();
+    console.log(city);
+    try {
+      const data = await opencage.geocode({ q: city, key: import.meta.env.VITE_OPENCAGE_API_KEY });
+      if (data.status.code === 200 && data.results.length > 0) {
+        const place = data.results[0];
+        setLocation({
+          lat: place.geometry.lat,
+          lng: place.geometry.lng
+        });
+        getTheaters(place.geometry.lat,place.geometry.lng);
+      } else {
+        console.log("Status", data.status.message);
+        console.log("total_results", data.total_results);
+      }
+    } catch (error) {
+      console.log("Error", error.message);
+      if (error.status.code === 402) {
+        console.log("hit free trial daily limit");
+        console.log("become a customer: https://opencagedata.com/pricing");
+      }
+    }
+    
   };
+  const currentDate = new Date();
 
+  const year = currentDate.getFullYear();
+  const month = String(currentDate.getMonth() + 1).padStart(2, '0');
+  const day = String(currentDate.getDate()).padStart(2, '0');
+  const hours = String(currentDate.getHours()).padStart(2, '0');
+  const minutes = String(currentDate.getMinutes()).padStart(2, '0');
+  const seconds = String(currentDate.getSeconds()).padStart(2, '0');
+
+  const filmId = '364605';
+  
+    const getTheaters = async (lat,lng)=>{
+      try{
+        const res = await fetch(`https://api-gate2.movieglu.com/cinemasNearby/?n=30`,{
+            method: 'GET',
+            headers:{
+              'client' : 'MOVI_227',
+              'x-api-key': import.meta.env.VITE_X_API_KEY,
+              'authorization': import.meta.env.VITE_AUTHORIZATION,
+              'territory': 'IN',
+              'api-version': 'v200',
+              'geolocation':`${lat};${lng}`,
+              'device-datetime': `${year}-${month}-${day}T${hours}:${minutes}:${seconds}Z`
+            },
+
+        });
+        const data = await res.json();
+        if (res.ok){
+          console.log(data.cinemas);
+          const filteredTheater = data.cinemas.filter(theat => theat.city === city);
+          setTheater(filteredTheater);
+        }
+        else{
+          console.error('HTTP error:', res.status, res.statusText);
+          console.error('Response body:', data);
+        }
+      }catch(error){
+        console.log(error.message);
+        
+      }
+    };
+    
+
+    const handleactiveTheater = (idx) => {
+      // console.log(key, buttonValues.key);
+      setActiveTheater(idx);
+      const selectedTheater = theater[idx];
+    if (selectedTheater.showings && selectedTheater.showings.Standard) {
+      setShowTimmings(selectedTheater.showings.Standard.times);
+    } else {
+      setShowTimmings([]);
+      console.log("No showings available for the selected theater");
+    }
+    };
+    const handleTimmingButton = (key)=>{
+        setActiveTime(key);
+
+    }
+    
   return (
     <div className="main">
       <h1>Book Now</h1>
-      <form
-        onSubmit={() => {
-          setTheater(true);
-        }}
-      >
-        <input type="text" placeholder=" city names" />
+      <form onSubmit={getGeoLocation}>
+        <input type="text" placeholder=" city names" id="city" value={city} onChange={handleButtonClick} />
         <button type="submit">Search</button>
       </form>
-      {theater && <div className="theater">Theater</div>}
-      <div className="time-slots">
-        {Object.entries(buttonValues).map(([key, value]) => (
-          <button
-            key={key}
-            onClick={() => handleButtonClick(key)}
+      
+
+
+      <footer>
+        <Link to="/seating">
+          <button>select seat</button>
+        </Link>
+      </footer>
+      <p>{city}
+        {location.lat}
+        {location.lng}
+      </p>
+      <div>
+      {theater && (
+          theater.map((theater, idx) => (
+            <button key={idx}
+            onClick={() => handleactiveTheater(idx)}
             style={{
-              backgroundColor: activeButton === key ? "blue" : "gray",
+              backgroundColor: activeTheater === idx ? "blue" : "gray",
               color: "white",
               margin: "5px",
             }}
-          >
-            {value}
-          </button>
-        ))}
+            >
+              {theater.cinema_name},{theater.address}</button>
+          ))
+        )}
       </div>
-      <footer>
-        <Link to="/seating">
-          <button>Select Seat</button>
-        </Link>
-      </footer>
+      <div>
+      {showTimmings && (
+          showTimmings.map((timming, idx) => (
+            <button key={idx}
+            onClick={() => handleTimmingButton(idx)}
+            style={{
+              backgroundColor: activeTime === idx ? "blue" : "gray",
+              color: "white",
+              margin: "5px",
+            }}
+            >
+              {timming.start_time}</button>
+          ))
+        )}
+      </div>
     </div>
   );
 };
---
+++ frontend/src/components/TheaterDropdown/TheaterDropdown.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const TheaterDropdown = () => {
+  return (
+    <div>TheaterDropdown</div>
+  )
+}
+
+export default TheaterDropdown
