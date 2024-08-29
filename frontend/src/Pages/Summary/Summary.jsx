7ac744eece556266bc657791eafa309d9dd1bbce
Vecforge <devansh.ps002@gmail.com>
2024-08-29 23:05:46 +0530

    Home Page Update

--- frontend/src/Pages/Summary/Summary.jsx
+++ frontend/src/Pages/Summary/Summary.jsx
@@ -1,17 +1,18 @@
-import React, { useState,useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
+import { Link } from "react-router-dom";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faArrowLeft } from '@fortawesome/free-solid-svg-icons';
 import './Summary.css';
 
 const Summary = () => {
-
-  const mv_id = useParams();
-
+  const { id } = useParams(); // Destructure the id from useParams
   const [mv_details, setMv_details] = useState([]);
 
   useEffect(() => {
     const getMovies = async () => {
       try {
-        const response = await fetch(`https://api.themoviedb.org/3/movie/${mv_id.id}?api_key=${import.meta.env.VITE_TMDB_API_KEY}`);
+        const response = await fetch(`https://api.themoviedb.org/3/movie/${id}?api_key=${import.meta.env.VITE_TMDB_API_KEY}`);
         const data = await response.json();
         setMv_details(data);
       } catch (error) {
@@ -20,37 +21,32 @@ const Summary = () => {
     };
 
     getMovies();
-  }, []);
+  }, [id]);
 
   console.log(mv_details);
   
   return (
-  <div className="sd-home">
-    This is summary page.
-    <div className="sd-container">
-      <div className="sd-poster">
-        <img src={`https://image.tmdb.org/t/p/w500${mv_details.poster_path}`} alt="" className="sd-img"/>
-    </div>
-    <div className="sd-caption">
-      <h3 className="sd-title">
-        Title Here
-      </h3>
-      <div className="sd-genre">
-        genre here
+    <div 
+      className="sd-home"
+      style={{ backgroundImage: `url(https://image.tmdb.org/t/p/original${mv_details.backdrop_path})` }}
+    >
+      <Link to ='/'>
+      <FontAwesomeIcon icon={faArrowLeft} className="sd-back"/>
+      </Link>
+      <div className="sd-container">
+        <div className="sd-poster">
+          <img src={`https://image.tmdb.org/t/p/w500${mv_details.poster_path}`} alt={mv_details.title} />
+        </div>
+        <div className="sd-caption">
+          <h3 className="sd-title">{mv_details.title}</h3>
+          <div className="sd-genre">genre here</div>
+          <p>{mv_details.overview}</p>
+          <div className="sd-btn">buttons here</div>
+        </div>
       </div>
-      <p>Overview here</p>
-      <div className="sd-btn">
-        buttons here
-      </div>
+      <div className="sd-cast">Cast Cards</div>
     </div>
-  </div>
-
-  <div className="sd-cast">
-    Cast Cards
-  </div>
-
-</div>
-)
+  );
 };
 
 export default Summary;
