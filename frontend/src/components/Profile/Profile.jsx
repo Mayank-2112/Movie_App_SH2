9829b71805b2b7a3cac8990446fc3f0b96286d1d
Mayank2112 <30808mayank21@gmail.com>
2024-08-31 16:46:53 +0530

    added Profile page of the user

--- frontend/src/components/Profile/Profile.jsx
+++ frontend/src/components/Profile/Profile.jsx
@@ -1,25 +1,57 @@
-import React from "react";
-// import { useAuth0 } from "@auth0/auth0-react";
+import React, { useState } from 'react'
+import './Profile.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCircleXmark } from '@fortawesome/free-solid-svg-icons';
+import { useDispatch, useSelector } from 'react-redux';
+import {signOutSuccess,signOutFailure} from '../../redux/user/userSlice.js';
 
-const Profile = () => {
-  // const { user, isAuthenticated, isloading } = useAuth0();
-  
+const Profile = ({onClose}) => {
+    const {currentUser} = useSelector((state)=> state.user);
+    const [formData, setFormData] = useState({});
+    const dispatch = useDispatch();
 
+    const handleChange = (e) => {
+        setFormData({ ...formData, [e.target.id]: e.target.value });
+      };
+      const handleSignOut = async()=>{
+        try {
+          const res = await fetch('/backend/user/signout',{
+            method: 'POST'
+          });
+          const data = await res.json();
+          if(res.ok){
+            onClose();
+            dispatch(signOutSuccess());
+          }
+          else{
+            console.log(data.message);
+          }
+        } catch (error) {
+          console.log(error.message);
+        }
+      };
+    
   return (
-  <div>
-    {/* <>
-    {isAuthenticated && (
-          <div className="profile-pic">
-            <img src={user.picture} />
-          </div>
-        )}
-    </> */}
+    <div className='profile'>
+        <FontAwesomeIcon icon={faCircleXmark} onClick={onClose}  className='close'/>
+        <div className="pr-main">
+            <h1>User Profile</h1>
+            <img src={currentUser.profilePicture} alt={currentUser.fullname} className='pr-img'/>
+            <form>
+                <input type="text" placeholder='FullName' id='fullname' onChange={handleChange} value={currentUser.fullname}/>
+                <input type="email" placeholder='Email' id='email' onChange={handleChange} value={currentUser.email}/>
+                <input type="text" placeholder='City' id='city' onChange={handleChange} value={currentUser.city}/>
+                <input type="password" placeholder='Password' id='password' onChange={handleChange} value={currentUser.password} />
+                <button className='btn' type='submit'>Update Profile</button>
+            </form>
+            <div className="func">
+                <p className='delete'>Delete Account</p>
+                <p className='logOut' onClick={handleSignOut}>LogOut</p>
+            </div>
+        </div>
 
-    {/* Below section executes when isAthenticated value is false (for testing purposes)*/}
-    <> 
-      <img className="profile-pic" src="" alt="Profile" />
-    </>
-  </div>);
-};
+    </div>
+  )
+}
 
-export default Profile;
+export default Profile
--- frontend/src/components/Profile/Proflie.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const Proflie = () => {
-  return <div></div>;
-};
-
-export default Proflie;
