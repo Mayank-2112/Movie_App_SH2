7fa87b88d243e40b73056a73550dd3b77bbd3bcb
Vecforge <devansh.ps002@gmail.com>
2024-08-31 19:03:09 +0530

    Summary Page Updated

--- frontend/src/Pages/Summary/Summary.jsx
+++ frontend/src/Pages/Summary/Summary.jsx
@@ -12,6 +12,7 @@ const Summary = () => {
   const [mv_details, setMv_details] = useState([]);
   const [ytkey, setYtkey] = useState([]);
   const [cast, setCast] = useState([]);
+  const [mv_genre, setMv_genre] = useState([]);
 
   useEffect(() => {
     const getMovies = async () => {
@@ -19,6 +20,7 @@ const Summary = () => {
         const response = await fetch(`https://api.themoviedb.org/3/movie/${id}?api_key=${import.meta.env.VITE_TMDB_API_KEY}`);
         const data = await response.json();
         setMv_details(data);
+        setMv_genre(data.genres); // Make sure this is "genres" instead of "genre"
       } catch (error) {
         console.error('Error fetching movies:', error);
       }
@@ -41,7 +43,6 @@ const Summary = () => {
     getCast();
   }, [id]);
 
-
   useEffect(() => {
     const getYTKey = async () => {
       try {
@@ -56,58 +57,56 @@ const Summary = () => {
     getYTKey();
   }, [id]);
 
-  // console.log(ytkey[0]);
-  console.log(mv_details);
-  
   return (
     <>
-    <div 
-      className="sd-home"
-      style={{ backgroundImage: `url(https://image.tmdb.org/t/p/original${mv_details.backdrop_path})`}}
-    >
-    </div>
+      <div 
+        className="sd-home"
+        style={{ backgroundImage: `url(https://image.tmdb.org/t/p/original${mv_details.backdrop_path})`}}
+      >
+      </div>
 
-    <div className="sd-above">
-    <Link to ='/'>
-      <FontAwesomeIcon icon={faArrowLeft} className="sd-back-button"/>
-      </Link>
-      <div className="sd-container">
-        <div className="sd-poster">
-          <img src={`https://image.tmdb.org/t/p/w500${mv_details.poster_path}`} alt={mv_details.title} />
+      <div className="sd-above">
+        <Link to='/'>
+          <FontAwesomeIcon icon={faArrowLeft} className="sd-back-button"/>
+        </Link>
+        <div className="sd-container">
+          <div className="sd-poster">
+            <img src={`https://image.tmdb.org/t/p/w500${mv_details.poster_path}`} alt={mv_details.title} />
+          </div>
+          <div className="sd-caption">
+            <h3 className="sd-title">{mv_details.title}</h3>
+            <div className="sd-genre">
+              <FontAwesomeIcon icon={faStar} className="sd-star" />
+              <p className="rating">{(Math.floor(mv_details.vote_average * 10) / 10).toFixed(1)}</p>
+              {mv_genre && mv_genre.length > 0 && mv_genre.map((genre, indexg) => (
+                <div className="gen" key={indexg}>
+                  <p className="gen1">{genre.name}</p>
+                </div>
+              ))}
+            </div>
+            <p className="sd-overview">{mv_details.overview}</p>
+            <div className="sd-btn">
+              <Link to='/booknow'>
+                <button className="btn-bknow"><FontAwesomeIcon icon={faPlay} />Book Now</button>
+              </Link>
+            </div>
+          </div>
         </div>
-        <div className="sd-caption">
-          <h3 className="sd-title">{mv_details.title}</h3>
-          <div className="sd-genre">
-          <FontAwesomeIcon icon={faStar} className="sd-star" /><p className="rating">{(Math.floor(mv_details.vote_average * 10)/10).toFixed(1)}</p>
-          {mv_details.genres.map((genre, indexg) => (
-            <div className="gen" key = {indexg}>
-            <p className="gen1">{genre.name}</p>
-          </div>
-          ))}
-          </div>
-          <p className="sd-overview">{mv_details.overview}</p>
-          <div className="sd-btn">
-            <Link to='/booknow'>
-          <button className="btn-bknow"><FontAwesomeIcon icon={faPlay} />Book Now</button>
-          </Link>
-          </div>
+
+        <div className="watch-trailer">
+          <h1>Watch Trailer</h1>
         </div>
-      </div>
 
-      <div className="watch-trailer">
-        <h1>Watch Trailer</h1>
-      </div>
+        <div className="sd-trailer">
+          <Player yt_key={ytkey[0]} />
+        </div>
 
-      <div className="sd-trailer">
-        <Player yt_key={ytkey[0]}/>
-      </div>
-
-      <div className="cast-ttle">
-        <h1>Cast</h1>
-      </div>
-      <div className="sd-cast">
-        <Cast castList = {cast}/>
-      </div>
+        <div className="cast-ttle">
+          <h1>Cast</h1>
+        </div>
+        <div className="sd-cast">
+          <Cast castList={cast} />
+        </div>
       </div>
     </>
   );
