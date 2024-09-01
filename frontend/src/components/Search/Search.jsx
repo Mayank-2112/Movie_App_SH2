1ab018d547ab21b43bc9abea004ae8e718a9ee23
Mayank2112 <30808mayank21@gmail.com>
2024-09-01 17:13:12 +0530

    changes in search box

--- frontend/src/components/Search/Search.jsx
+++ frontend/src/components/Search/Search.jsx
@@ -7,23 +7,30 @@ function Search() {
 
   const [input, setInput] = useState("");
   
-  const fetchData = (value) =>{
-    fetch(`https://api.themoviedb.org/3/search/movie?query=${encodeURIComponent(value)}&include_adult=false&language=en-US&page=1&api_key=${import.meta.env.VITE_TMDB_API_KEY}`)
-    .then(response => response.json)
-    .then(json => console.log(json))
-    .catch(err => console.error(err))
+  const fetchData = async (value) =>{
+    try {
+      const response = await fetch(`https://api.themoviedb.org/3/search/movie?query=${encodeURIComponent(value)}&include_adult=false&language=en-US&page=1&api_key=${import.meta.env.VITE_TMDB_API_KEY}`)
+      const data = await response.json();
+      console.log(data);
+      
+    } catch (error) {
+      console.log(error);
+      
+    }
   }
 
 //Twisters
   
-  const handleClick = () =>{
+  const handleClick = (e) =>{
+    e.preventDefault();
     fetchData(input);
   }
     
   return (
     <>
     <div className="display">
-            <form action="" className="s-form" onSubmit={handleClick}><input type="text" id="passw" placeholder="Search..." value={input} onChange={(e) => setInput(e.target.value)}/>
+            <form className="s-form" onSubmit={handleClick}>
+            <input type="text" id="passw" placeholder="Search..." value={input} onChange={(e) => setInput(e.target.value)}/>
             <FontAwesomeIcon icon={faMagnifyingGlass} />
             </form>
         </div>
