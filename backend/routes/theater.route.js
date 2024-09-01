33e90cb81e0818dc7646988c1e99e5eeb587fb93
Mayank2112 <30808mayank21@gmail.com>
2024-09-01 18:44:34 +0530

    theater updates deleted

--- backend/routes/theater.route.js
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-import { saveTheater } from '../controllers/theater.controller.js';
-
-const router = express.Router();
-
-router.post('/add',saveTheater);
-
-export default router;
