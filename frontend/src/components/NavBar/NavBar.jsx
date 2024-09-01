d7eb18aaf1ffc94230bf92f0b1aba17675cba1f4
Vecforge <devansh.ps002@gmail.com>
2024-09-01 20:16:36 +0530

    NavBar Black on Scroll

--- frontend/src/components/NavBar/NavBar.jsx
+++ frontend/src/components/NavBar/NavBar.jsx
@@ -1,75 +1,16 @@
 import React, { useState, useEffect } from "react";
-import Search from "../Search/Search";
-import Profile from "../Profile/Profile";
 import { Link } from "react-router-dom";
 import './NavBar.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faBars, faCaretDown, faUser, faRightToBracket} from '@fortawesome/free-solid-svg-icons';
-import {useDispatch, useSelector} from 'react-redux';
-import {signOutSuccess,signOutFailure} from '../../redux/user/userSlice.js';
+import { faRightToBracket } from '@fortawesome/free-solid-svg-icons';
+import { useDispatch, useSelector } from 'react-redux';
+import { signOutSuccess } from '../../redux/user/userSlice.js';
+import Search from "../Search/Search";
 
-// function Navbar() {
-//   const [click, setClick] = useState(true);
-//   const [button, setButton] = useState(true);
-//   const [navbar, setNavbar] = useState(false);
-
-//   // const handleClick = () => setClick(!click);
-//   const closeMobileMenu = () => setClick(false);
-
-//   return (
-//     <>
-//     <nav className="navbar">
-//       <div className="site-title">
-//         <Link to='/' >FlickScribe</Link>
-//         </div>
-//         {/* <div className="menu-icon" onClick={handleClick}></div> */}
-//         {/* <ul className={click ? "nav-menu active" : "nav-menu"}> */}
-//         {/* <ul className="nav-menu"> */}
-        
-//           <div className="nav-item i1">
-//             <Link to='/' onClick={closeMobileMenu}>
-//             Movies
-//             </Link>
-//           </div>
-          
-//           {/* <li className="nav-item">
-//             <Link to='/' className="nav-links" onClick={closeMobileMenu}>
-//             Contact
-//             </Link>
-//           </li> */}
-
-//           <div className="nav-item i2">
-//             <Link to='/'  onClick={closeMobileMenu}>
-//             About_Us
-//             </Link>
-//           </div>
-
-//           <div className="nav-item i3">
-//             <Link to='/' onClick={closeMobileMenu}>
-//             Support
-//             </Link>
-//           </div>
-          
-//           <div className="search">
-//             <Search />
-//           </div>
-
-//           <div className="profile">
-//             <Profile />
-//           </div>
-          
-//         {/* </ul> */}
-//     </nav>
-//     </>
-//   );
-// }
-
-// export default Navbar;
-
-function Navbar({onProfileClick}) {
+function Navbar({ onProfileClick, result }) {
   const dispatch = useDispatch();
-  const {currentUser} = useSelector((state) =>state.user);
-  // console.log(currentUser);
+  const { currentUser } = useSelector((state) => state.user);
+  const [scrollNav, setScrollNav] = useState(false);
 
   const handleDropdown = () => {
     const selectElement = document.querySelector(".select-item");
@@ -82,21 +23,32 @@ function Navbar({onProfileClick}) {
     }
   };
 
+  const changeNavBackground = () => {
+    if (window.scrollY >= 80) {
+      setScrollNav(true);
+    } else {
+      setScrollNav(false);
+    }
+  };
+
   useEffect(() => {
+    window.addEventListener("scroll", changeNavBackground);
     window.addEventListener("click", handleOutsideClick);
-    return () => window.removeEventListener("click", handleOutsideClick);
+    return () => {
+      window.removeEventListener("scroll", changeNavBackground);
+      window.removeEventListener("click", handleOutsideClick);
+    };
   }, []);
 
-  const handleSignOut = async()=>{
+  const handleSignOut = async () => {
     try {
-      const res = await fetch('/backend/user/signout',{
+      const res = await fetch('/backend/user/signout', {
         method: 'POST'
       });
       const data = await res.json();
-      if(res.ok){
+      if (res.ok) {
         dispatch(signOutSuccess());
-      }
-      else{
+      } else {
         console.log(data.message);
       }
     } catch (error) {
@@ -104,10 +56,8 @@ function Navbar({onProfileClick}) {
     }
   };
 
-
-
   return (
-    <div className="navbar">
+    <div className={`navbar ${scrollNav ? 'navbar-scrolled' : ''}`}>
       <div className="navbar-left">
         <Link to="/" className="site-title">FlixCribe</Link>
         <ul>
@@ -116,29 +66,23 @@ function Navbar({onProfileClick}) {
         </ul>
       </div>
       <div className="navbar-right">
-        <Search />
+        <Search sitem={result} />
       </div>
-      {/* <div className="navbar-profile">
-        <FontAwesomeIcon icon={faUser} className="profile" />
-        <FontAwesomeIcon icon={faCaretDown}/>
-        <div className="dropdown">
-          <p>LogOut</p>
-        </div>
-      </div> */}
       {currentUser ? (
         <div className="selector">
           <div className="select" onClick={handleDropdown}>
-            <img src={currentUser.profilePicture} alt="user" /></div>
+            <img src={currentUser.profilePicture} alt="user" />
+          </div>
           <div className="select-item">
             {currentUser.isAdmin ? (
               <div className="selected-item trans">Dashboard</div>
-            ):(
+            ) : (
               <div className="selected-item" onClick={onProfileClick}>Profile</div>
             )}
             <div className="selected-item" onClick={handleSignOut}>Logout</div>
           </div>
         </div>
-      ):(
+      ) : (
         <Link to='/login'>
           <button className="Btn-login"><FontAwesomeIcon icon={faRightToBracket} />Login</button>
         </Link>
